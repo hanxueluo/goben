e1c47ebb14a4e704799f51f257de5fd9d13dee26
Huanle Han <hanhuanle@caicloud.io>
2020-05-27 11:39:29 +0800

    refactor build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,48 @@
 #!/bin/sh -e
 
-export GOOS=linux
 
-gofmt -s -w ./goben
-go tool fix ./goben
-go vet ./goben
-
-#which gosimple >/dev/null && gosimple ./goben
-which golint >/dev/null && golint ./goben
-#which staticcheck >/dev/null && staticcheck ./goben
-
-#go test ./goben
-#CGO_ENABLED=0 go install -v ./goben
 mkdir -p ./bin/
 rm -f ./bin/*
 
-CGO_ENABLED=0 go build -o ./bin/goben -v ./goben
+build_local() {
+    gofmt -s -w ./goben
+    go tool fix ./goben
+    go vet ./goben
 
-docker build -f Dockerfile -t 'hanxueluo/goben-server:latest' .
+    #which gosimple >/dev/null && gosimple ./goben
+    which golint >/dev/null && golint ./goben
+    #which staticcheck >/dev/null && staticcheck ./goben
+
+    #go test ./goben
+    #CGO_ENABLED=0 go install -v ./goben
+
+    CGO_ENABLED=0 go build -o ./bin/goben -v ./goben
+}
+
+build_image() {
+    docker build -f Dockerfile -t 'hanxueluo/goben-server:latest' .
+}
+
+build_linux() {
+    export GOOS=linux
+    build_local
+}
+
+target=$1
+
+if [ "$target" = "run" ];then
+    build_local
+    ./bin/goben
+    exit 0
+fi
+
+if [ "$target" = "linux" ];then
+    build_linux
+    exit 0
+fi
+
+if [ "$target" = "" ];then
+    build_linux
+    build_image
+    exit 0
+fi
